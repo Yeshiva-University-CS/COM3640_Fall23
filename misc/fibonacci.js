95e893916b62ef7647196393c7db0676a29a0516
sacknovi <sacknovitz@gmail.com>
2023-12-06 14:59:45 -0500

    added fibonacci.js

---
+++ misc/fibonacci.js
@@ -0,0 +1,10 @@
+function fibonacci(n) {
+    if (n <= 1) return n;
+    let a = 0, b = 1, sum;
+    for (let i = 2; i <= n; i++) {
+        sum = a + b;
+        a = b;
+        b = sum;
+    }
+    return b;
+}
